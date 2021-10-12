8e7013ab1a2e95ab14dfea9b1ed261dc7a679513
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-10-11 19:33:48 -0600

    added styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './App.css';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -10,13 +11,11 @@ function App() {
     <>
     <Router>
       <Header />
-
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/projects' component={Projects} />
       </Switch>
-
-    <Footer />
+      <Footer />
     </Router>
     </>
   )
