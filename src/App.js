30f8e62bd8433bc35dab8d8c9ed7bdd1783dbc35
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-10-11 18:52:42 -0600

    added projects page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import * as React from 'react';
 import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Navbar from './components/Header';
+import Header from './components/Header';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <>
     <Router>
-      <Navbar />
+      <Header />
+
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/projects' component={Projects} />
       </Switch>
+
     <Footer />
     </Router>
     </>
