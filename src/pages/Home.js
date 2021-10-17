d810d41aca0123c5a82f696e4ada352cb6b54d8c
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-10-16 22:24:51 -0600

    updated styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
     root: {
-      background: '#682ae9',
+      background: 'linear-gradient(45deg, #682ae9 30%, #FE6B8B 90%)',
       border: 0,
       borderRadius: 3,
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
